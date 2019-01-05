5b69ce56a23810c33c3a495fb0819923eef10ac8
Diegoisawesome <diego@domoreaweso.me>
2019-01-05 01:42:46 -0600

    Rough Violet City

---
+++ tileset_palettes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+import os, subprocess
+
+with open(argv[1], 'rb') as f:
+    f.seek(int(argv[2], base=16), 0)
+    data = f.read(512)
+
+for i in range(0, 16):
+    filename = os.sep.join([argv[3], format(i, '02')])
+    with open(filename + '.gbapal', 'wb+') as f:
+        f.write(data[i * 32:i * 32 + 32])
+    subprocess.call(["C:\\CrystalDust\\tools\\gbagfx\\gbagfx", filename + '.gbapal', filename + '.pal'])
