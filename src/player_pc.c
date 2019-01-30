ebe521ae77a94f10501dd842648b7a8094219617
Diegoisawesome <diego@domoreaweso.me>
2019-01-30 00:01:24 -0600

    Add credits.txt and remove potion from player's PC at game start

--- src/player_pc.c
+++ src/player_pc.c
@@ -169,7 +169,7 @@ static const struct MenuAction gPCText_ItemPCOptionsText[] =
 
 static const struct ItemSlot gNewGamePCItems[] =
 {
-    { ITEM_POTION, 1 },
+    //{ ITEM_POTION, 1 },
     { ITEM_NONE, 0 }
 };
 
