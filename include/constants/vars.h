c765a0dccc0767924dd23590b85fbc32c496c1d6
Diegoisawesome <diego@domoreaweso.me>
2019-10-12 16:15:36 -0700

    Minor script changes

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -47,7 +47,7 @@
 #define VAR_RECYCLE_GOODS                    0x4020
 #define VAR_REPEL_STEP_COUNT                 0x4021
 #define VAR_ICE_STEP_COUNT                   0x4022
-#define VAR_STARTER_MON                      0x4023 // 0=Treecko, 1=Torchic, 2=Mudkip
+#define VAR_STARTER_MON                      0x4023 // 0=Chikorita, 1=Totodile, 2=Cyndaquil
 #define VAR_MIRAGE_RND_H                     0x4024
 #define VAR_MIRAGE_RND_L                     0x4025
 #define VAR_SECRET_BASE_MAP                  0x4026
