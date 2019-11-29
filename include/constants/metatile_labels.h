60b357c4320621a18599be1f65c2b4c2cf917ec6
Diegoisawesome <diego@domoreaweso.me>
2019-11-29 03:12:49 -0600

    Scripts for Kyle's house, begin Earl's academy

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -179,9 +179,7 @@
 #define METATILE_General_TallGrass_TreeUp     0x025
 
 // gTileset_GenericBuilding
-#define METATILE_GenericBuilding_TableEdge               0x2F1
-#define METATILE_GenericBuilding_TrickHouse_Door_Closed  0x21B
-#define METATILE_GenericBuilding_TrickHouse_Stairs_Down  0x219
+#define METATILE_GenericBuilding_TableEdge  0x2F1
 
 // gTileset_InsideOfTruck
 #define METATILE_InsideOfTruck_DoorClosedFloor_Bottom  0x21D
