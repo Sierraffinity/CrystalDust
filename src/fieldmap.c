330cd898aafdd23cdc5c0f313edc856b41d7fa33
Sierraffinity <sierra@domoreaweso.me>
2020-12-26 18:28:48 -0800

    Fix undefined behavior causing crashes on staircases and in Pokédex on My Boy!

--- src/fieldmap.c
+++ src/fieldmap.c
@@ -747,8 +747,25 @@ bool8 CameraMove(int x, int y)
 struct MapConnection *sub_8088950(u8 direction, int x, int y)
 {
     int count;
-    struct MapConnection *connection;
     int i;
+    struct MapConnection *connection;
+    const struct MapConnections *connections = gMapHeader.connections;
+    // UB: Multiple possible null dereferences
+#ifdef UBFIX
+    if (connections != NULL)
+    {
+        count = connections->count;
+        connection = connections->connections;
+        if (connection != NULL)
+        {
+            for (i = 0; i < count; i++, connection++)
+            {
+                if (connection->direction == direction && sub_80889A8(direction, x, y, connection) == TRUE)
+                    return connection;
+            }
+        }
+    }
+#else
     count = gMapHeader.connections->count;
     connection = gMapHeader.connections->connections;
     for (i = 0; i < count; i++, connection++)
@@ -756,6 +773,7 @@ struct MapConnection *sub_8088950(u8 direction, int x, int y)
         if (connection->direction == direction && sub_80889A8(direction, x, y, connection) == TRUE)
             return connection;
     }
+#endif
     return NULL;
 }
 
