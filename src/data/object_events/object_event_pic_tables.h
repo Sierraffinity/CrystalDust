515c8283a1a8cbc2c7028d86b25207ddab148964
Deokishisu <6993375+Deokishisu@users.noreply.github.com>
2022-04-11 19:03:43 -0400

    Give Teala & Union Room Nurse Walking Frames
    The Union Room nurse needed it because she walks to retrieve the Odd Egg.
    
    Also added for Teala to futureproof for potential other uses/the Battle Frontier.

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -934,12 +934,12 @@ static const struct SpriteFrameImage sPicTable_Teala[] = {
     overworld_frame(gObjectEventPic_Teala, 2, 4, 0),
     overworld_frame(gObjectEventPic_Teala, 2, 4, 1),
     overworld_frame(gObjectEventPic_Teala, 2, 4, 2),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 0),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 0),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 1),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 1),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 2),
-    overworld_frame(gObjectEventPic_Teala, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 3),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 4),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 5),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 6),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 7),
+    overworld_frame(gObjectEventPic_Teala, 2, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_BreakableRock[] = {
@@ -2227,12 +2227,12 @@ static const struct SpriteFrameImage sPicTable_UnionRoomAttendant[] = {
     overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 0),
     overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 1),
     overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 2),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 0),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 0),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 1),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 1),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 2),
-    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 2),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 3),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 4),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 5),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 6),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 7),
+    overworld_frame(gObjectEventPic_UnionRoomAttendant, 2, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Sudowoodo[] = {
